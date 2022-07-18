7d88e2d4073e66216c292ae26dea2929b554d233
shivabalakrishnak <106053909+shivabalakrishnak@users.noreply.github.com>
2022-07-18 21:38:58 +0530

    UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
 
 import "./index.scss";
 
@@ -7,13 +8,9 @@ import C360Home from "./C360Home";
 
 const App = () => (
   <div>
-  <header className="bg-green-600 text-white p-3 font-mono text-3xl">
-    Customer 360
-  </header>
+  <Header></Header>
   <C360Home></C360Home>
-  <footer className="bg-green-600 text-white p-2 font-mono ">
-    Copyrights @ HGS Digital
-  </footer>
+  
 </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
